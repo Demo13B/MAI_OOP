f1c42246c751242e2b2e36ec05d8e53480b91385
TRSalikhov <trsalikhov@mai.education>
2023-09-25 21:01:01 +0300

    Reversed the constructors

--- LW2/main.cpp
+++ LW2/main.cpp
@@ -3,10 +3,10 @@
 
 auto main() -> int {
     try {
-        Seven num = Seven("0211");
-        Seven num2 = Seven("0201");
+        Seven num = Seven({'1', '2', '0'});
+        Seven num2 = Seven("1020");
         bool res = num == num2;
-        std::cout << res;
+        std::cout << num2.get_value();
     } catch (const std::invalid_argument& ex) {
         std::cout << "Invalid argument exception caught: " << ex.what() << std::endl;
     }
