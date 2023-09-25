af7c1be94c203c98f0844b0dd5108871c960e0fe
TRSalikhov <trsalikhov@mai.education>
2023-09-25 18:02:00 +0300

    Added > and < operators

--- LW2/main.cpp
+++ LW2/main.cpp
@@ -4,9 +4,9 @@
 auto main() -> int {
     try {
         Seven num = Seven("0211");
-        Seven num2 = Seven("0");
-        Seven res = num - num2;
-        std::cout << num.get_value() << std::endl;
+        Seven num2 = Seven("0201");
+        bool res = num > num;
+        std::cout << res;
     } catch (const std::invalid_argument& ex) {
         std::cout << "Invalid argument exception caught: " << ex.what() << std::endl;
     }
