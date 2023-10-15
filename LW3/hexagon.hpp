3a1363cc91bdc0594c0dfcd85bdc425920671172
TRSalikhov <trsalikhov@mai.education>
2023-10-15 23:08:26 +0300

    Added hexagon.hpp

--- LW3/hexagon.hpp
+++ LW3/hexagon.hpp
@@ -3,36 +3,36 @@
 #include <vector>
 #include "figure.hpp"
 
-class Pentagon : public Figure {
+class Hexagon : public Figure {
    private:
     std::vector<std::pair<double, double>> _points;
     std::pair<double, double> _mid_point;
     double _surface;
 
    public:
-    Pentagon();
-    Pentagon(std::vector<std::pair<double, double>>& points);
-    ~Pentagon();
+    Hexagon();
+    Hexagon(std::vector<std::pair<double, double>>& points);
+    ~Hexagon();
 
     auto get_points() const -> std::vector<std::pair<double, double>>;
     auto mid_point() const -> std::pair<double, double> override;
     auto surface() const -> double override;
 
-    friend auto operator<<(std::ostream& os, const Pentagon& p) -> std::ostream&;
-    friend auto operator>>(std::istream& is, Pentagon& p) -> std::istream&;
+    friend auto operator<<(std::ostream& os, const Hexagon& p) -> std::ostream&;
+    friend auto operator>>(std::istream& is, Hexagon& p) -> std::istream&;
 
-    auto operator==(const Pentagon& other) const -> bool;
-    auto operator=(const Pentagon& other) -> Pentagon&;
+    auto operator==(const Hexagon& other) const -> bool;
+    auto operator=(const Hexagon& other) -> Hexagon&;
 
     explicit operator double() const override;
 };
 
-inline auto operator<<(std::ostream& os, const Pentagon& p) -> std::ostream& {
+inline auto operator<<(std::ostream& os, const Hexagon& p) -> std::ostream& {
     os << "Points: ";
 
     std::vector<std::pair<double, double>> points = p.get_points();
 
-    for (size_t i = 0; i != 5; ++i) {
+    for (size_t i = 0; i != 6; ++i) {
         os << "x" << i + 1 << " = " << points[i].first << ", "
            << "y" << i + 1 << " = " << points[i].second << "; ";
     }
@@ -42,14 +42,14 @@ inline auto operator<<(std::ostream& os, const Pentagon& p) -> std::ostream& {
     return os;
 }
 
-inline auto operator>>(std::istream& is, Pentagon& p) -> std::istream& {
-    std::vector<std::pair<double, double>> v(5);
+inline auto operator>>(std::istream& is, Hexagon& p) -> std::istream& {
+    std::vector<std::pair<double, double>> v(6);
 
-    for (size_t i = 0; i != 5; ++i) {
+    for (size_t i = 0; i != 6; ++i) {
         is >> v[i].first >> v[i].second;
     }
 
-    p = Pentagon(v);
+    p = Hexagon(v);
 
     return is;
 }
