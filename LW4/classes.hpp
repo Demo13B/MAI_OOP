347a12a2f1ba6f53c5afc3b85acc14b5c5100c1c
TRSalikhov <trsalikhov@mai.education>
2023-10-28 19:41:19 +0300

    Classes done

--- LW4/classes.hpp
+++ LW4/classes.hpp
@@ -1,4 +1,6 @@
+#pragma once
 #include <utility>
+#include <vector>
 
 class Figure {};
 
@@ -9,7 +11,7 @@ class Pentagon : public Figure {
 
     Pentagon()
         : size(5),
-          points() {}
+          points({{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}) {}
 
     Pentagon(std::vector<std::pair<T, T> >& v)
         : size(5),
@@ -17,35 +19,42 @@ class Pentagon : public Figure {
 
     ~Pentagon() {
         size = 0;
-        delete points;
     }
 };
 
 template <typename T>
 class Hexagon : public Figure {
     size_t size;
-    std::pair<T, T> points;
+    std::vector<std::pair<T, T> > points;
 
     Hexagon()
         : size(6),
-          points() {}
+          points({{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}) {}
 
     Hexagon(std::vector<std::pair<T, T> >& v)
         : size(6),
           points(v) {}
+
+    ~Hexagon() {
+        size = 0;
+    }
 };
 
 template <typename T>
 class Octagon : public Figure {
    public:
     size_t size;
-    std::pair<T, T> points;
+    std::vector<std::pair<T, T> > points;
 
     Octagon<T>()
         : size(8),
-          points() {}
+          points({{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}) {}
 
     Octagon<T>(std::vector<std::pair<T, T> >& v)
         : size(8),
           points(v) {}
+
+    ~Octagon() {
+        size = 0;
+    }
 };
--- LW4/main.cpp
+++ LW4/main.cpp
@@ -2,6 +2,7 @@
 #include "classes.hpp"
 
 int main() {
-    Octagon<double> o = Octagon<double>();
-    std::cout << o.size;
+    std::vector<std::pair<int, int> > v = {{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, {8, 8}};
+    Octagon<int> o = Octagon<int>(v);
+    std::cout << o.points[3].first << ' ' << o.points[3].second << std::endl;
 }
