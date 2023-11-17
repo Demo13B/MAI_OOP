849b569b9569729178f8ee6ace9a91702c433dc6
TRSalikhov <trsalikhov@mai.education>
2023-11-17 19:52:54 +0300

    First version of allocator

--- LW5/main.cpp
+++ LW5/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <map>
+#include "allocator.hpp"
 
 auto main() -> int {
-    std::cout << "It works!" << std::endl;
+    std::map<int, int, std::less<int>, mai::Allocator<std::pair<const int, int>>> m;
+
+    m[1] = 10;
+    m[2] = 20;
+    m[3] = 30;
+
+    std::cout << "1: " << m[1] << " 2: " << m[2] << "3: " << m[3] << std::endl;
     return 0;
 }
