905e9f07ac6b0b34537924f0ab78e236c1d799d9
TRSalikhov <trsalikhov@mai.education>
2023-12-16 23:45:26 +0300

    Added factory

---
+++ LW6/factory.cpp
@@ -0,0 +1,36 @@
+#include "factory.hpp"
+#include "dragon.hpp"
+#include "elf.hpp"
+#include "wandering_knight.hpp"
+
+std::shared_ptr<NPC> factory(std::istream& in) {
+    std::string type, name;
+    int x, y;
+    char c;
+    in >> type >> name >> c >> x >> c >> y >> c;
+    std::shared_ptr<NPC> res;
+    if (type == "Dragon") {
+        res = std::make_shared<Dragon>(x, y, name);
+    } else if (type == "Wandering_Knight") {
+        res = std::make_shared<Wandering_Knight>(x, y, name);
+    } else if (type == "Elf") {
+        res = std::make_shared<Elf>(x, y, name);
+    } else if (type != "") {
+        std::cerr << "Unknown type" << std::endl;
+    }
+    return res;
+}
+
+std::shared_ptr<NPC> factory(const std::string& type, const std::string& name, const int& x, const int& y) {
+    std::shared_ptr<NPC> res;
+    if (type == "Dragon") {
+        res = std::make_shared<Dragon>(x, y, name);
+    } else if (type == "Wandering_Knight") {
+        res = std::make_shared<Wandering_Knight>(x, y, name);
+    } else if (type == "Elf") {
+        res = std::make_shared<Elf>(x, y, name);
+    } else {
+        std::cerr << "Unknown type" << std::endl;
+    }
+    return res;
+}
